9f343db1f016a388b4e5682be381991840d0a00c
mgnavarrete <mgnavarrete@uc.cl>
2025-02-24 13:23:21 -0300

    noline

--- corrector.py
+++ corrector.py
@@ -307,9 +307,10 @@ if __name__ == '__main__':
             resetMD(img_names, metadata_path, 'all')
             saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             linesList = findFlights(path_root, folder_path, img_names, geonp_path, transformer)
-            correctYawLine(folder_path, geonp_path, metadata_path, metadatanew_path, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb, linesList)
+            correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
+            # correctYawLine(folder_path, geonp_path, metadata_path, metadatanew_path, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb, linesList)
 
-            saveKMLFlights(linesList, path_root, 'Yaw')
+            saveKMLFlights(linesList, path_root, 'YawNoLine')
             saveGeoM(img_names, metadata_path, geonp_path, path_root) 
             correctHLine(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, ancho, areaUmb, path_root, linesList)
             saveKMLFlights(linesList, path_root, 'Yaw_H')
