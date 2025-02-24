ea21c8a323e635ba5ec17aa4aa3032d64f529e09
mgnavarrete <mgnavarrete@uc.cl>
2025-02-24 16:18:23 -0300

    save

--- corrector.py
+++ corrector.py
@@ -304,8 +304,8 @@ if __name__ == '__main__':
         
         elif planta == '8':
             print("Ajustando Planta Don Humberto...")
-            # resetMD(img_names, metadata_path, 'all')
-            # saveGeoM(img_names, metadata_path, geonp_path, path_root)   
+            resetMD(img_names, metadata_path, 'all')
+            saveGeoM(img_names, metadata_path, geonp_path, path_root)   
             linesList = findFlights(path_root, folder_path, img_names, geonp_path, transformer)
             # correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             correctYawLine(folder_path, geonp_path, metadata_path, metadatanew_path, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb, linesList)
