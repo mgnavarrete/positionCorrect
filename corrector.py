8a4a3ba841f0739d07f01fc2c54fcd977c5425ba
mgnavarrete <mgnavarrete@uc.cl>
2025-02-24 13:25:19 -0300

    noline

--- corrector.py
+++ corrector.py
@@ -310,13 +310,13 @@ if __name__ == '__main__':
             correctYawLLK(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb)
             # correctYawLine(folder_path, geonp_path, metadata_path, metadatanew_path, transformer, model, yawKML, ancho, list_images, areaUmb, difUmb, linesList)
 
-            saveKMLFlights(linesList, path_root, 'YawNoLine')
+            saveKMLFlights(linesList, path_root, 'Yaw_NOLINE')
             saveGeoM(img_names, metadata_path, geonp_path, path_root) 
             correctHLine(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, ancho, areaUmb, path_root, linesList)
-            saveKMLFlights(linesList, path_root, 'Yaw_H')
+            saveKMLFlights(linesList, path_root, 'Yaw_H_NOLINE')
             saveGeoM(img_names, metadata_path, geonp_path, path_root) 
             correctELine(folder_path, img_names, geonp_path, metadata_path, metadatanew_path, df, transformer, model, path_root, linesList)
-            saveKMLFlights(linesList, path_root, 'Yaw_H_E')
+            saveKMLFlights(linesList, path_root, 'Yaw_H_E_NOLINE')
             deleteGeoNp(geonp_path)
 
     print("Todas la carpetas OK")
